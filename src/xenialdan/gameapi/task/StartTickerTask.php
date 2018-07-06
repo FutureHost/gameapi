2c97514ec1a5805381c2bf1afff3dd198b770106
Eren Ahmed Akyol <ahmederen123@gmail.com>
2018-07-06 11:15:02 +0300

    Fixs

--- src/xenialdan/gameapi/task/StartTickerTask.php
+++ src/xenialdan/gameapi/task/StartTickerTask.php
@@ -1,26 +1,39 @@
 <?php
 
+/**
+*  _____                    ____   ___    __     ___  
+* | ____| _ __  ___  _ __  | ___| / _ \  / /_   / _ \ 
+* |  _|  | '__|/ _ \| '_ \ |___ \| (_) || '_ \ | | | |
+* | |___ | |  |  __/| | | | ___) |\__, || (_) || |_| |
+* |_____||_|   \___||_| |_||____/   /_/  \___/  \___/ 
+* 
+* @version v2
+* @author Eren5960
+* @link https://github.com/Eren5960/JobForm
+*/                  
+
 namespace xenialdan\gameapi\task;
 
 use pocketmine\plugin\Plugin;
-use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 use xenialdan\gameapi\Arena;
 
-class StartTickerTask extends PluginTask{
+class StartTickerTask extends Task{
 
 	private $arena;
-
+	public $plugin;
+	
 	public function __construct(Plugin $plugin, Arena $arena){
-		parent::__construct($plugin);
+		$this->plugin = $plugin;
 		$this->arena = $arena;
 	}
 
 	public function onRun(int $currentTick){
 		if ($this->arena instanceof Arena){
-			$this->arena->sendTimer($this->getOwner());
+			$this->arena->sendTimer($this->plugin);
 			return;
 		} else
-			$this->getOwner()->getServer()->getScheduler()->cancelTask($this->getTaskId());
+			$this->plugin->getScheduler()->cancelTask($this->getTaskId());
 		return;
 	}
 }
